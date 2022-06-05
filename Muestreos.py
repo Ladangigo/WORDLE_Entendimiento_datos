de3814673d9296e760a4a2b59336aa214bce6770
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-05 15:53:51 -0500

    Se realiza un muestreo de los datos ya aptos para el banco de palabras

--- Muestreos.py
+++ Muestreos.py
@@ -6,6 +6,7 @@ MuestreoDatos = [ ]
 
 lista_de_archivos = os.listdir("raw_texts/")
 
+#Muestreo de datos dados por el cliente
 for archivo in lista_de_archivos:
     with open('raw_texts/' + archivo, 'r', encoding='utf8') as archivo_total:
         archivo_original = archivo_total.read()
@@ -19,3 +20,13 @@ print(MuestreoDatos)
 
 with open('uniondearchivos.txt', 'w', encoding='utf8') as f:
     f.write(''.join(Archivos)) #Se unen todos los archivos en uno solo
+
+#Muestreo de datos ya una vez pasado por un proceso de limpieza
+with open('/content/Data/final_resume_file.txt', 'r',encoding="utf8") as raw_file:
+    archivo = raw_file.read()
+    final_resume_file = archivo.split(' ')
+    muestra = int(len(final_resume_file)/10)
+    MuestreoDatosLimpios = random.sample(final_resume_file, muestra)
+
+print(MuestreoDatosLimpios)
+  
