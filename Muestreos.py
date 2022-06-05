3ae234868aa16a637e73924bf7ac67c1f731eca2
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-05 15:49:27 -0500

    Se crea el muestreo de datos de cada uno de los archivos

--- Muestreos.py
+++ Muestreos.py
@@ -1,3 +1,18 @@
 import random
 import os
 
+Archivos = []
+MuestreoDatos = [ ]
+
+lista_de_archivos = os.listdir("raw_texts/")
+
+for archivo in lista_de_archivos:
+    with open('raw_texts/' + archivo, 'r', encoding='utf8') as archivo_total:
+        archivo_original = archivo_total.read()
+        Archivos.append(archivo_original)
+        listArchivo = archivo_original.split(' ')
+        Muestra = int(len(listArchivo)/10) #Se hace un muestreo del 10% de los datos de cada archivo
+        MuestreoDato = random.sample(listArchivo, Muestra)
+        MuestreoDatos.append(MuestreoDato)
+        
+print(MuestreoDatos)
