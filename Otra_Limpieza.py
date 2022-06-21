5261ce4d0f53e242075b13476bd35959040a01e5
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-21 10:42:20 -0500

    Se unen todos los archivos dados por el cliente en uno solo

--- Otra_Limpieza.py
+++ Otra_Limpieza.py
@@ -5,10 +5,27 @@ Created on Sun Jun 12 13:26:58 2022
 @author: Usuario
 """
 
-with open('uniondearchivos.txt', 'r',encoding="utf8") as raw_file:
-    original_file = raw_file.read()
+import random
+import os
+files = []
 
-txt = original_file.split(' ')
 
+list_of_files = os.listdir("raw_texts/")
 
-print(txt)
+#Se unen todos los archivos en uno solo 
+for file in list_of_files:
+    with open('raw_texts/' + file, 'r', encoding='utf8') as rawfile:
+        raw_files = rawfile.read()
+        files.append(raw_files)
+
+        
+with open('uniondearchivos.txt', 'w', encoding='utf8') as f: #Se crea un archivo dondese van a unir todos los .txt
+    f.write(''.join(files))
+    
+
+with open('uniondearchivos.txt', 'r', encoding='utf8') as file: #Se abre el archivo donde estan todos los .txt unidos
+    r_file = file.read()
+    raw_file_join = r_file.split(' ')
+    
+    
+    
