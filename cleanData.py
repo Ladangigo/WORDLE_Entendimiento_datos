217c5570e650123d9b77eda402a3ed3b378b4b18
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-20 23:17:13 -0500

    Se termina de organizar orden de limpieza.

--- cleanData.py
+++ cleanData.py
@@ -7,8 +7,7 @@ import os
 nltk.download('stopwords')
 stop_words = stopwords.words('spanish')
 
-file_with_uppercase = []
-file_without_uppercase = []
+
 dic_files = {}
 final_resume_file = []
 resume_file = []
@@ -21,7 +20,7 @@ def CleanData(namefile):
     #1.Elimina todos los carácteres o síbolos
     #2. Se eliminan palabras con mas de dos mayúsculas
     #3. Se cambian vocales con tíldes por sus homólogos sin estas
-    #4. Se eliminan los dígitos númericos 
+    #4. Se eliminan los dígitos númericos y otros acentos 
     #5. Se eliminan palabras duplicadas 
     #6. Se eliminan palabras con una longitud menor a 3
     #Una vez realizado todo este proceso de limpieza de datos, se crea una carpeta la cual 
@@ -54,30 +53,29 @@ def CleanData(namefile):
     texto = re.sub(r"ó","o", texti)
     textu = re.sub(r"ú","u", texto)
 
-
-    file_with_stopwords = re.findall(r'[aA-zZñÑ]+', file_lowercase)
+    #Solo letras
+    file_without_numbers = [ ]
+    file_without_numbers = re.findall(r'[aA-zZÑñ]+', textu)
       
+    #Eliminar duplicados
+    file_without_duplicates = [ ]
+    file_without_duplicates = list(OrderedDict.fromkeys(file_without_numbers))   
     
-    #Elimino los stop words
+    
+    #Elimino stopwprds
     file_without_stopwords = []
-    for item in file_with_stopwords:
-        if not item in stop_words:
-            file_without_stopwords.append(item)
+    for item in file_without_duplicates:
+      if not item in stop_words:
+        file_without_stopwords.append(item)
+
+    #se eliminan las palabraas menores a 4 letras
+    clean_file=file_without_stopwords.copy()
+    word= None
+    for word in file_without_stopwords:
+      if len(word)<4:
+        clean_file.remove(word)    
     
-    #Elimino palabras duplicadas
-    file_without_duplicates = list(OrderedDict.fromkeys(file_without_stopwords))
-    
-    #Elimino las palabras con lingitud menor a 4
-    short_words = []
-    for word in file_without_duplicates:
-        if len(word)<4:
-            short_words.append(word)
-    
-    #Se eliminan las stop words
-    clean_file = []
-    for count in file_without_duplicates:
-        if count not in short_words:
-            clean_file.append(count)
+
             
     #Se crea un nuevos archivos de texto para cada file procesado            
     with open(F'datos procesados/{namefile.split("/")[1]}procesado.txt', 'w', encoding="utf8" ) as f:
