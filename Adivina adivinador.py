db4275f4657b0102034feccc1483b0b474cc86d7
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-27 16:31:31 -0500

    Se escriben los métodos de la primera clase

--- Adivina adivinador.py
+++ Adivina adivinador.py
@@ -6,7 +6,31 @@ Created on Mon Jun 27 13:35:06 2022
 """
 
 class Update_words():
-    pass
+            
+    def __init__(self, APIget_response_length, database, APIpost_response = None):
+        self.APIget_response_length = APIget_response_length
+        self.APIpost_response = APIpost_response
+        self.database = database
+ 
+    def filter_words_according_APIget (self):
+          self.words_game = [ ]
+          self.words_game = self.database[str(self.APIget_response_length)]
+        
+        
+    def filter1():
+        pass
+    
+    def filter2():
+        pass
+    
+    def filter3():
+        pass
+           
+    
+    def update_APIpost():
+        pass
+
+   
 
 class Select_words():
     pass
