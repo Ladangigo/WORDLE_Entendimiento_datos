08c4bd594f65d20b78cca9466896b603899bb646
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-27 17:36:32 -0500

    Se crean dos metodos de la clase sistema 1. carga archivo, 2. organizar bd como dataframe según long, cantidad de vocales y cons

--- Adivina adivinador.py
+++ Adivina adivinador.py
@@ -4,17 +4,23 @@ Created on Mon Jun 27 13:35:06 2022
 
 @author: Laura
 """
+import pandas as pd
+import numpy as np
 
 class Update_words():
             
-    def __init__(self, APIget_response_length, database, APIpost_response = None):
+    def __init__(self, APIget_response_length,APIget_number_vowels, database, APIpost_response = None):
         self.APIget_response_length = APIget_response_length
+        self.APIget_number_vowels = APIget_number_vowels
         self.APIpost_response = APIpost_response
         self.database = database
+        self.filter_words_according_APIget()
  
     def filter_words_according_APIget (self):
-          self.words_game = [ ]
-          self.words_game = self.database[str(self.APIget_response_length)]
+          self.words_game = self.database.loc[np.logical_and(self.database['longitud'] == int(self.APIget_response_length),
+                                                             self.database['Vocal'] == int(self.APIget_number_vowels))]
+          
+          
         
         
     def filter1():
@@ -39,4 +45,28 @@ class Game():
     pass
 
 class Sistem():
-    pass
+    
+    def __init__(self, route):
+        self.route = route
+        
+    def open_file(self, route):
+        self.words_bank = [ ]
+        with open(route, 'r', encoding='utf8') as words:
+              word_bank = words.read()
+        self.words_bank = word_bank.split(' ')
+        
+    def position_vowels_cons(self):
+        dictToDF_wordInfo = {'Palabra':[],'Consonante':[], 'Vocal':[], 'Longitud': []}
+
+        #Conocer el numero de vocales y consonantes de una palabra
+        for word in self.words_bank:
+          numberVowels = len([l for l in word if l in 'aeiou'])
+          numberCons = len(word) - numberVowels
+          length = len(word)
+          dictToDF_wordInfo['Palabra'].append(word)
+          dictToDF_wordInfo['Consonante'].append(numberCons)
+          dictToDF_wordInfo['Vocal'].append(numberVowels)
+          dictToDF_wordInfo['Longitud'].append(length)
+        
+        self.DF_wordInfo = pd.DataFrame(dictToDF_wordInfo)
+
