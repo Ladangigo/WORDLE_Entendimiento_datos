3d2bda6fc26674689dccfbac50dcb0bcc8779ae8
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-27 14:14:02 -0500

    Se crean las clases que componen la aplicación

--- Adivina adivinador.py
+++ Adivina adivinador.py
@@ -2,6 +2,17 @@
 """
 Created on Mon Jun 27 13:35:06 2022
 
-@author: Usuario
+@author: Laura
 """
 
+class Update_words():
+    pass
+
+class Select_words():
+    pass
+
+class Game():
+    pass
+
+class Sistem():
+    pass
