1e363c89c77dfe9cd42b010f14c3bb3406d9946a
Laura Gil <ldaniela.ggomez@gmail.com>
2022-06-27 21:53:35 -0500

    Se crean dos métodos, 1. calcula los patrones de cada palabra. 2. selecciona las palabras con el patrón de mayor incidencia

--- Adivina adivinador.py
+++ Adivina adivinador.py
@@ -40,8 +40,49 @@ class Update_words():
    
 
 class Select_words():
-    pass
+    
+    def __init__(self, database):
+        self.database_game = database
+        
+        
+    def calculate_pattern(self):
+          word_length = len(self.database_game['Palabra'][0])
 
+          newDF_dict = {'Palabra':[]}
+          for newKey in range(word_length):
+            newDF_dict[f'posicion {str(newKey)}'] = [ ]
+          
+          for index, length in enumerate(self.database_game['Palabra']):
+            word = self.database_game['Palabra'][index]
+            newDF_dict['Palabra'].append(word)
+        
+            for positionVowel in self.database_game['posicionVocal'][index]:
+              newDF_dict[f'posicion {str(positionVowel)}'].append(1)#1 para vocales
+            for positionCons in self.database_game['posicionCons'][index]:
+              newDF_dict[f'posicion {str(positionCons)}'].append(0)#0 para Consonante
+        
+          self.words_patterns = pd.DataFrame(newDF_dict)
+        
+    # Se selecciona el patrón con mayor incidencia       
+    def select_pattern(self):
+          resume_patterns = self.words_patterns[self.words_patterns.columns[1:]].groupby(
+              self.words_patterns[self.words_patterns.columns[1:]].columns.tolist(),as_index=False).size()
+  
+          pattern_more_often = resume_patterns['size'].max()
+        
+          filter_pattern=resume_patterns['size']==pattern_more_often
+          positions = np.flatnonzero(filter_pattern)
+          higher_incidence_pattern = resume_patterns.iloc[positions]
+          
+          select_patterns_higher_incidence = self.words_pattern.join(higher_incidence_pattern.set_index(list(higher_incidence_pattern.columns.values[:-1])), 
+                                        on=list(self.words_pattern.columns.values[1:]), how='right')
+          self.words_select_patterns_higher_incidence = list(select_patterns_higher_incidence['Palabra'].values)
+          
+          
+    def probabilities_letter_in_position(self):
+        pass
+
+       
 class Game():
     
     def __init__(self,database):
@@ -74,8 +115,10 @@ class Sistem():
         self.words_bank = word_bank.split(' ')
         
     def position_vowels_cons(self):
-        dictToDF_wordInfo = {'Palabra':[],'Consonante':[], 'Vocal':[], 'Longitud': []}
-
+        dictToDF_wordInfo = {'Palabra':[],'Consonante':[],'posicionCons': [], 'Vocal':[],'posicionVocal':[], 'Longitud': []}
+        vowels =[]
+        cons = []
+        
         #Conocer el numero de vocales y consonantes de una palabra
         for word in self.words_bank:
           numberVowels = len([l for l in word if l in 'aeiou'])
@@ -85,6 +128,17 @@ class Sistem():
           dictToDF_wordInfo['Consonante'].append(numberCons)
           dictToDF_wordInfo['Vocal'].append(numberVowels)
           dictToDF_wordInfo['Longitud'].append(length)
+          
+          for position,letter in enumerate(word):
+            if letter in 'aeiou':
+              vowels.append(position)
+            else:
+              cons.append(position)
+                
+          dictToDF_wordInfo['posicionVocal'].append(vowels)
+          dictToDF_wordInfo['posicionCons'].append(cons)
+          vowels =[]
+          cons = []
         
         self.DF_wordInfo = pd.DataFrame(dictToDF_wordInfo)
         
